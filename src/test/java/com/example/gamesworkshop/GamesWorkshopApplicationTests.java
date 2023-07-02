12bd1b3335a46dd37744e9a9bb716859e0433c28
BulbaBeggins <Bumerang>
2023-07-02 13:54:22 +0400

    Init2

---
+++ src/test/java/com/example/gamesworkshop/GamesWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamesworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
